e5e62361c51d77c3990e5d501e1c1b05893c1201
Darsh3008 <darshkanani326@gmail.com>
2026-07-13 22:09:20 +0530

    fix: Refactor CORS configuration to use allowed origins array

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,8 +3,24 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
-app.use(cookieParser());  
-app.use(cors({origin: 'https://interview-ai-one-sigma.vercel.app', credentials: true}));     
+app.use(cookieParser());
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://interview-ai-one-sigma.vercel.app",
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+}));
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("API is running successfully 🚀")
@@ -12,5 +28,5 @@ app.get("/", (req, res) => {
 const authRouter = require("./routes/auth.routes.js")
 const interviewRouter = require("./routes/interview.routes.js")
 app.use("/api/auth", authRouter)
-app.use("/api/interview" , interviewRouter)
+app.use("/api/interview", interviewRouter)
 module.exports = app
